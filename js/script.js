4070d204c7236eaf0b38fad91348d9a87b02147f
sumaiyahassan2007 <sumaiyahassan2007@gmail.com>
2026-08-11 20:01:11 +0600

    Update website

---
+++ js/script.js
@@ -0,0 +1,12 @@
+
+
+$('.slides').slick({
+  slidesToShow: 4,
+  slidesToScroll: 1,
+  autoplay: true,
+  autoplaySpeed: 1000,
+  prevArrow : '<i class="fa-solid fa-circle-left prev"></i>',
+  nextArrow : '<i class="fa-solid fa-circle-right next"></i>',
+});
+
+
