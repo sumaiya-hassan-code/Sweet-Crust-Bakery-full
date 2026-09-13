997f3c2ce29992b27e2f1e13be8b49863b0c4610
sumaiyahassan2007 <sumaiyahassan2007@gmail.com>
2026-09-14 02:28:08 +0600

    Update Website

--- js/script.js
+++ js/script.js
@@ -1,12 +1,67 @@
 
-
 $('.slides').slick({
-  slidesToShow: 4,
-  slidesToScroll: 1,
-  autoplay: true,
-  autoplaySpeed: 1000,
-  prevArrow : '<i class="fa-solid fa-circle-left prev"></i>',
-  nextArrow : '<i class="fa-solid fa-circle-right next"></i>',
+    slidesToShow: 4,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 1000,
+
+    prevArrow: '<i class="fa-solid fa-circle-left prev"></i>',
+    nextArrow: '<i class="fa-solid fa-circle-right next"></i>',
+
+    responsive: [
+        {
+            breakpoint: 1024,
+            settings: {
+                slidesToShow: 3
+            }
+        },
+        {
+            breakpoint: 768,
+            settings: {
+                slidesToShow: 2
+            }
+        },
+        {
+            breakpoint: 480,
+            settings: {
+                slidesToShow: 1
+            }
+        }
+    ]
 });
+
+// Mobile menu
+let menuBtn = document.getElementById("menuBtn");
+let mobileMenu = document.getElementById("mobileMenu");
+
+menuBtn.addEventListener("click", () => {
+    mobileMenu.classList.toggle("hidden");
+});
+
+let mobileLinks = document.querySelectorAll(".mobile-link");
+
+mobileLinks.forEach(link => {
+    link.addEventListener("click", () => {
+        mobileMenu.classList.add("hidden");
+    });
+});
+
+
+let navLinks = document.querySelectorAll(".nav-link");
+
+navLinks.forEach(link => {
+
+    link.addEventListener("click", () => {
+
+        navLinks.forEach(item => {
+            item.classList.remove("border-b-2", "pb-2" , "border-[#A65C00]");
+        });
+
+        link.classList.add("border-b-2", "border-[#A65C00]");
+
+    });
+
+});
+
 
 
